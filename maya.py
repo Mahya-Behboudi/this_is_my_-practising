64c576672d2a385bcb04f750a44161e5cca0fda6
Mahya Beh <mahyab79@gmail.com>
2021-05-29 23:52:41 +0430

    imported two modules (random and has_clashes)

---
+++ maya.py
@@ -0,0 +1,15 @@
+# what is the shiffel work!
+import random
+from  chess_solution import has_clashes
+def main():
+    found_num = 0
+    tries = 0
+    while found_num < 10:
+        l = list(range(8))
+        random.shuffle(l)
+        tries +=1
+        if not has_clashes(l):
+            print("found solution {0} in {1} tries".format(l,tries))
+            tries = 0
+            found_num +=1
+main()
